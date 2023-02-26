a5f1ca2f160e9e322a6deb96ccbc420aae8b6bf8
Agustín Gómez <a.g.lamela@gmail.com>
2023-02-26 10:56:02 +0100

    subiendo mains de los retos

---
+++ retos/reto.conectaLab/SOLUCION/Main1.java
@@ -0,0 +1,8 @@
+public class Main1 {
+
+    public static void main(String[] args) {
+        int ancho=5, pisos = 3;
+        String caracteres = "**";
+	    System.out.println(RetoA.crearArbol(ancho, pisos, caracteres));
+    }
+}
