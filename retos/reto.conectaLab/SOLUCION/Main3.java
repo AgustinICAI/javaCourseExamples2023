7e8cbacb87a7ed1f8518fd1d9b7dfdb506d54d6b
Agustín Gómez <a.g.lamela@gmail.com>
2023-02-26 10:53:57 +0100

    subiendo soluciones practica

--- retos/reto.conectaLab/SOLUCION/Main3.java
+++
@@ -1,8 +0,0 @@
-public class Main3 {
-    public static void main(String argv[]){
-        Persona persona = new Persona();
-        persona.setNombre("Luis")
-                .setEdad(22)
-                .setDireccion("Calle del Pez, 17");
-    }
-}
